7c1471a4e65c1c73ccbd7f55c9a08afd6b154dd2
Tyler Collins <advent14@gmail.com>
2016-01-10 22:56:00 -0500

    Pre help era

--- MainProject/ChunkMapper.cpp
+++ MainProject/ChunkMapper.cpp
@@ -21,15 +21,27 @@ glm::vec3 ChunkMapper::getOpenChunk(glm::vec3 charPos) {
 
 	glm::vec2 mappedPos = mapToMultiple((int)(charPos.x / SCALING_FACTOR), (charPos.z / SCALING_FACTOR));
 
-	//use of static vars over each?
-
 	for (TerrainChunk& tc : *terrainListRef) { // Check all of the chunks that are actually drawing for the pos below the char.
 		if (tc.isDrawing()) {
 			if (glm::vec2(tc.getCenterX(), tc.getCenterY()) == mappedPos) {
 				return glm::vec3(0.0f);
 			}
+			//if -> check one above middle
+			//	 ->	returns true? -> return null vec
 		}
 	}
-
+	///VVVVVVVVVVVVVVVV
 	return glm::vec3(mappedPos.x, 0, mappedPos.y);
 }
+
+//int chunkMapHelper[] = { // Mapper which contains the offset of chunks that should
+//	-8, 8,
+//	0, 8, // Top row
+//	8, 8
+//	- 8, 0,
+//	0, 0, // Middle check first.
+//	8, 0
+//	- 8, -8,
+//	0, -8, // Middle check first.
+//	8, -8
+//}
