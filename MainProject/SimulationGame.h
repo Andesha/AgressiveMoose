3088623bcfcde59c026e38ad57318103c5f76485
Tyler Collins <advent14@gmail.com>
2016-01-11 02:35:34 -0500

    Done the last major comment thing. More could probably be added to the later classes but thats not a problem for now.

--- MainProject/SimulationGame.h
+++ MainProject/SimulationGame.h
@@ -8,55 +8,112 @@
 #include "Character.h"
 #include "Skybox.h"
 
-// Main class which specifies all behaviour of our game engine.
+/// <summary>
+/// Main class for the entire project. All behaviour is controlled from here.
+/// </summary>
 class SimulationGame {
 private:
-	SDL_Window* window; // Window information. Possibly auto-detect later.
+
+	/// Below are SDL variables.
+	SDL_Window* window;
 	int windowWidth;
 	int windowHeight;
 	float fps;
 	float frameTime;
 
+	/// Reference to the TerrainList containing the chunks.
 	TerrainList* terrainList;
 
+	/// Default shader for the chunks.
 	GLSLProgram program;
 
-	GameState gameState; // See GameState.h
+	/// See GameState.h
+	GameState gameState; 
 
+	/// Reference to the character for drawing/camera/model behaviours.
     Character* character;
 
+	/// Skybox for the project. Contains all of its own inits and drawing methods.
 	Skybox* skybox;
-
+	
+	/// Perlin object for generating and building our terrain.
 	Perlin perlin;
 
-	void initialize(); // Builds all objects like OpenGL contexts and our window.
+	/// <summary>
+	/// Builds all objects like OpenGL contexts and our window.
+	/// </summary>
+	void initialize();
 
-	void initializeShaders(); // Call all needed attributes of the GLSL program.
+	/// <summary>
+	/// Call all needed attributes of the GLSL program. 
+	/// </summary>
+	void initializeShaders();
 
-	void examineInput(); // SDL input polling.
+	/// <summary>
+	/// SDL input polling.
+	/// </summary>
+	void examineInput();
 
-	void gameLoop(); // Method to be called after initialize has finished.
+	/// <summary>
+	/// Standard game loop which will call for input polling.
+	/// </summary>
+	void gameLoop();
 
-	void drawWorld(); // Method call for drawing all objects we need.
+	/// <summary>
+	/// Call to draw and render all of our objects.
+	/// </summary>
+	void drawWorld();
 
-	void calculateFPS(); // Examine the frame ticks via SDL. Eventually needed for delta time.
+	/// <summary>
+	/// Do the math required for calculating the average in the circular buffer.
+	/// </summary>
+	void calculateFPS();
 
+	/// <summary>
+	/// Bind the default ground texture.
+	/// </summary>
 	void makeTestTexture();
 
+	/// <summary>
+	/// Helper method for cleaning up the game loop when computing fps.
+	/// </summary>
+	/// <param name="startMarker">Marker in ticks from when we last counted.</param>
 	void fpsCaretaker(float startMarker);
 
+	/// <summary>
+	/// Method for handling keys that start pressing down.
+	/// </summary>
     void handleKeyDown();
 
+	/// <summary>
+	/// Method for handling keys that finish pressing.
+	/// </summary>
     void handleKeyUp();
 
+	/// <summary>
+	/// Method for passing mouse updates to outside classes.
+	/// </summary>
+	/// <param name="mouseX">SDL relative mouse X.</param>
+	/// <param name="mouseY">SDL relative mouse Y.</param>
     void mouseUpdatePos(int mouseX, int mouseY);
 
-	bool inView(glm::mat4 M, glm::vec3 p);
-
 public:
+	/// <summary>
+	/// Default constructor containing all of the proper inits.
+	/// </summary>
 	SimulationGame();
+
+	/// <summary>
+	/// Never to be called destructor.
+	/// </summary>
 	~SimulationGame();
-  GLuint tid;
+
+	/// Texture ID for the ground.
+	GLuint tid;
+
+	/// <summary>
+	/// To be called for the main class to begin the project.
+	/// </summary>
 	void start(); // To be called to start the whole game.
 };
 
